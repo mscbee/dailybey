d9d22ce69bc8310f92fc692e3b37e5f832138885
mscbee <charlenebadibanga@gmail.com>
2018-04-13 20:37:52 +0100

    added Article model

--- beynews/dailybey/models.py
+++ beynews/dailybey/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    date =  models.DateField(format=api_settings.DATE_FORMAT)
+    time = models.TimeField(format=api_settings.TIME_FORMAT)
+    article = models.CharField()
+    picture = models.ImageField(allow_empty_file=True)
+    url = models.URLFieldmax_length=200, min_length=None, allow_blank=True)
+
