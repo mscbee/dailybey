ba94d210416bd8a8893d701a43f261e0d859fcb1
mscbee <charlenebadibanga@gmail.com>
2018-04-14 12:20:19 +0100

    created controller for listing all articles and single articles

--- beynews/dailybey/views.py
+++ beynews/dailybey/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from dailybey.models import Article
+from dailybey.serializers import ArticleSerializer
 
-# Create your views here.
+@csrf_exempt
+def list_all_articles(request):
+    if request.method == 'GET':
+        all_articles = Article.objects.all()
+        serializer = ArticleSerializer(all_articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def single_article_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        single_article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(single_article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(single_article, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        single_article.delete()
+        return HttpResponse(status=204)
