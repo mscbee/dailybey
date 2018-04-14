1f2395c74570c86a2136ec663f1032cb07436d00
mscbee <charlenebadibanga@gmail.com>
2018-04-14 11:58:47 +0100

    completed serializer for Article model

--- beynews/dailybey/models.py
+++ beynews/dailybey/models.py
@@ -6,7 +6,7 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     date =  models.DateField()
     time = models.TimeField()
-    article = models.CharField(max_length=5000)
-   # picture = models.ImageField(allow_empty_file=True)
+    body = models.CharField(max_length=5000)
+   # image = models.ImageField(allow_empty_file=True)
     url = models.URLField(max_length=200)
 
--- beynews/dailybey/serializers.py
+++ beynews/dailybey/serializers.py
@@ -2,11 +2,27 @@ from rest_framework import serializers
 from dailybey.models import Article
 
 class ArticleSerializer(serializers.Serializer):
-    title = models.CharField(max_length=100)
-    date =  models.DateField()
-    time = models.TimeField()
-    article = models.CharField(max_length=5000)
-   # picture = models.ImageField(allow_empty_file=True)
-    url = models.URLField(max_length=200)
+    title = serializers.CharField(max_length=100)
+    date = serializers.DateField()
+    time = serializers.TimeField()
+    body = serializers.CharField(max_length=5000)
+   # image = serializers.ImageField(allow_empty_file=True)
+    url = serializers.URLField(max_length=200)
 
-    #TODO: Continue with serializer
+    def create(self, validated_data):
+        """
+        Create and return a new Article instance, given the validated data.
+        """
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing Article instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.date = validated_data.get('date', instance.date)
+        instance.time = validated_data.get('time', instance.time)
+        #instance.image = validated_data.get('image', instance.image)
+        instance.body = validated_data.get('body', instance.body)
+        instance.save()
+        return instance
