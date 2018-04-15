4ba4aa3f11bfbd261ee2db5a00d1c842d745bd2a
mscbee <charlenebadibanga@gmail.com>
2018-04-15 15:56:17 +0100

    registered article model in admin.py and created superuser

--- beynews/dailybey/admin.py
+++ beynews/dailybey/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from dailybey.models import Article
 
 # Register your models here.
--- beynews/dailybey/models.py
+++ beynews/dailybey/models.py
@@ -6,7 +6,7 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     date =  models.DateField(auto_now_add=True, blank=True, null=True)
     time = models.TimeField(auto_now_add=True, blank=True, null=True)
-    body = models.CharField(max_length=5000)
+    body = models.CharField(max_length=5000, blank=True)
    # image = models.ImageField(allow_empty_file=True)
     url = models.URLField(max_length=200)
 
